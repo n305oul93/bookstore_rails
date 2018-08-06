71f739bbd8557a4d9ab1da6e5b42cb476bbedd52
D Pringle <dezmondp@gmail.com>
2018-08-05 22:10:03 -0500

    removed invalid migration for dropbooks.rb

--- db/migrate/20180701183117_dropbooks.rb
+++
@@ -1,7 +0,0 @@
-class Dropbooks < ActiveRecord::Migration[5.1]
-  def change
-    drop_table :books do |t|
-      t.remove :title, :category_id, :author_id, :publisher_id, :isbn, :year, :price, :buy, :excerpt, :format, :pages, :created_at, :updated_at
-    end
-  end
-end
